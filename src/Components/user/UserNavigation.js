768b0b11e0dc67eb528a547d8018b4902a69a97d
Ammobag <prithwishdey0321@gmail.com>
2021-08-03 11:57:18 +0530

    Added some styles to UserNavigation

--- src/Components/user/UserNavigation.js
+++ src/Components/user/UserNavigation.js
@@ -5,7 +5,6 @@ import { useLocation } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 
 import { Link } from "react-router-dom";
-import ErrorBoundary from "../functions/ErrorBoundary";
 import MenuIcon from "@material-ui/icons/Menu";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import Avatar from "@material-ui/core/Avatar";
@@ -47,19 +46,14 @@ export default function UserNavigation() {
 
   console.log(isNavOpen);
   return (
-    <ErrorBoundary>
-      <div>
-        <div
-          id="mySidenav"
-          className="sidenav"
-          style={isNavOpen ? openNav : closeNav}
-        >
-          <ArrowBackIcon
-            className="closebtn"
-            onClick={() => setIsNavOpen(false)}
-            style={{ cursor: "pointer", color: "#ffffff" }}
-          />
-
+    <div>
+      <div className="sidenav" style={isNavOpen ? openNav : closeNav}>
+        <ArrowBackIcon
+          className="closebtn"
+          onClick={() => setIsNavOpen(false)}
+          style={{ cursor: "pointer", color: "#ffffff" }}
+        />
+        <section>
           <Link to="/feed" style={{ textDecoration: "none" }}>
             <div>Feed</div>
           </Link>
@@ -79,38 +73,38 @@ export default function UserNavigation() {
           <Link to="/classifiedSection" style={{ textDecoration: "none" }}>
             <div>Classified Section</div>
           </Link>
-        </div>
+        </section>
+      </div>
 
-        <div className="navbar">
-          <div className="navbarContent">
-            <div className="hamburger">
-              <MenuIcon
-                onClick={() => setIsNavOpen(true)}
-                style={{
-                  color: "#ffffff",
-                }}
+      <div className="navbar">
+        <div className="navbarContent">
+          <div className="hamburger">
+            <MenuIcon
+              onClick={() => setIsNavOpen(true)}
+              style={{
+                color: "#ffffff",
+              }}
+            />
+          </div>
+          <div className="navbar-right">
+            {location.pathname === "/feed" && (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleAddPost}
+              >
+                Add Post
+              </Button>
+            )}
+            <div className="avatar">
+              <Avatar
+                alt="pic"
+                src="https://randomuser.me/api/portraits/med/men/58.jpg"
               />
-            </div>
-            <div className="navbar-right">
-              {location.pathname === "/feed" && (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleAddPost}
-                >
-                  Add Post
-                </Button>
-              )}
-              <div className="avatar">
-                <Avatar
-                  alt="pic"
-                  src="https://randomuser.me/api/portraits/med/men/58.jpg"
-                />
-              </div>
             </div>
           </div>
         </div>
       </div>
-    </ErrorBoundary>
+    </div>
   );
 }
