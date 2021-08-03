68a898d3d95775d2d5530d79911cbd998ad39e89
Ammobag <prithwishdey0321@gmail.com>
2021-08-03 09:50:03 +0530

    Resolved warnings

--- src/Components/user/UserNavigation.js
+++ src/Components/user/UserNavigation.js
@@ -1,10 +1,10 @@
 import { React, useState } from "react";
 import "./UserNavigation.css";
 import { useHistory } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 
-import { Switch, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ErrorBoundary from "../functions/ErrorBoundary";
 import MenuIcon from "@material-ui/icons/Menu";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
@@ -14,7 +14,6 @@ import firebase from "firebase/app";
 import "firebase/database";
 import "firebase/auth";
 import {} from "../firebase";
-import logout from "../functions/logout";
 
 export default function UserNavigation() {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -24,8 +23,7 @@ export default function UserNavigation() {
 
   const handleAddPost = (e) => {
     history.replace("/addPost");
-  }
-
+  };
 
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
@@ -94,11 +92,15 @@ export default function UserNavigation() {
               />
             </div>
             <div className="navbar-right">
-              {location.pathname==="/feed" && 
-                <Button variant="contained" color="primary" onClick={handleAddPost}>
+              {location.pathname === "/feed" && (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleAddPost}
+                >
                   Add Post
                 </Button>
-              }
+              )}
               <div className="avatar">
                 <Avatar
                   alt="pic"
--- src/Components/user/UserNoticeBoard.js
+++ src/Components/user/UserNoticeBoard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import UserNavigation from "./UserNavigation"
+import UserNavigation from "./UserNavigation";
 import "./style.css";
 import "./UserNoticeBoard.css";
 
@@ -9,7 +9,6 @@ import {} from "../firebase";
 import Getonce from "../functions/dbquery";
 
 export default function UserNoticeBoard() {
-
   var query = Getonce("notices/");
   var data = [];
 
@@ -17,19 +16,11 @@ export default function UserNoticeBoard() {
     if (Object.hasOwnProperty.call(query, key)) {
       const element = query[key];
 
-      var insert = {
-        col1: element.issuerName,
-        col2: element.issuerDesignation,
-        col3: element.subject,
-        col4: element.body,
-        col5: element.date,
-      };
-
       data.push(element);
     }
   }
 
-  const NoticeBoard = data.map((value) =>
+  const NoticeBoard = data.map((value) => (
     <div className="notice">
       <div className="info">
         <div className="issuer">
@@ -38,19 +29,15 @@ export default function UserNoticeBoard() {
         </div>
         <div className="date">{value.date}</div>
       </div>
-      <div className="subject">{value.subject}</div>  
-      <div className="message">
-        {value.body}
-      </div>
+      <div className="subject">{value.subject}</div>
+      <div className="message">{value.body}</div>
     </div>
-  );
+  ));
 
-  return( 
-    <div className="main-body"> 
-      <UserNavigation/>
-      <div className="feed-page">
-        {NoticeBoard}
-      </div>
+  return (
+    <div className="main-body">
+      <UserNavigation />
+      <div className="feed-page">{NoticeBoard}</div>
     </div>
   );
 }
