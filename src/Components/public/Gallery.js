df3eeaf82b91cef19df152433751f220f7134247
Ammobag <prithwishdey0321@gmail.com>
2021-09-01 16:25:18 +0530

    Obtained image Urls from firebase in gallery

--- src/Components/public/Gallery.js
+++ src/Components/public/Gallery.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Gallery.module.css";
 import slider1 from "../../Assets/slider1.jpeg";
 import slider2 from "../../Assets/slider2.jpeg";
 import slider3 from "../../Assets/slider3.jpeg";
 import slider4 from "../../Assets/slider4.jpeg";
+import firebase from "firebase";
 export default function Gallery() {
+  // Create a reference with an initial file path and name
+  var storageRef = firebase.storage().ref("gallery/").listAll();
+  useEffect(() => {
+    storageRef
+
+      .then((ListResult) => {
+        ListResult.items.forEach((element) => {
+          element.getDownloadURL().then((url) => console.log(url));
+        });
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.log("error:", error);
+      });
+  }, []);
   return (
     <div>
       <img alt="img" src={slider1} className={styles.img} />
